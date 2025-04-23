764ad19c4d47a00c64f04403a160fd0f0f38e8da
Van trong <trong.doan@tomosia.com>
2025-04-23 14:16:24 +0700

    fix: any bug

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,26 +8,4 @@ class Course < ApplicationRecord
   has_many :categories, through: :course_categories
 
   validates :title, :description, :price, :thumbnail_path, :language, :status, presence: true
-
-  scope :search_by_params, lambda { |params|
-    courses = all
-
-    courses = courses.where('LOWER(title) LIKE ?', "%#{params[:query].downcase}%") if params[:query].present?
-
-    courses = courses.where(status: params[:status]) if params[:status].present?
-
-    if params[:category_id].present?
-      courses = courses.joins(:course_categories).where(course_categories: { category_id: params[:category_id] })
-    end
-
-    if params[:price_type] == 'free'
-      courses = courses.where('price = 0 OR price IS NULL')
-    elsif params[:price_type] == 'paid'
-      courses = courses.where('price > 0')
-    end
-
-    courses = courses.where(user_id: params[:user_id]) if params[:user_id].present?
-
-    courses
-  }
 end
