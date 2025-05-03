50cbaeb6eaaa240032e17daa7b9ac9de19494c7a
Van trong <trong.doan@tomosia.com>
2025-05-04 00:40:02 +0700

    fix: mvc-video-index

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,5 +3,8 @@
 class Upload < ApplicationRecord
   belongs_to :user
 
-  validates :file_type, :cdn_url, :thumbnail_path, :duration, :resolution, :status, presence: true
+  validates :file_type, :cdn_url, :thumbnail_path, :duration, :resolution, presence: true
+
+  enumerize :status, in: %i[pending processing success failed], default: :pending, predicates: true, scope: true
+  enumerize :moderation_status, in: %i[pending approved rejected], default: :pending, predicates: true, scope: true
 end
