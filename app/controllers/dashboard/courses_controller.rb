778fe298e4c3d66713b9ce26d277a728bfd9d4dd
vantrong <trongdn2405@gmail.com>
2025-08-09 13:57:23 +0700

    refactor page course

--- app/controllers/dashboard/courses_controller.rb
+++ app/controllers/dashboard/courses_controller.rb
@@ -2,17 +2,12 @@
 
 class Dashboard::CoursesController < Dashboard::DashboardController
   before_action :set_course, only: %i[show edit update destroy]
+  before_action :set_categories, only: %i[index new edit update]
   before_action :initialize_course_service, only: %i[index show]
 
   def index
-    @categories = Category.all
-    @courses = @course_service.filter_and_paginate_courses(params)
-    
-    if params[:category_id] == 'all_categories'
-      @selected_category_name = 'All Categories'
-    elsif params[:category_id].present?
-      @selected_category_name = @categories.find_by(id: params[:category_id])&.name || 'Category'
-    end
+    @courses = @course_service.filter_and_paginate_courses(params).page(params[:page]).per(24)
+    @selected_category_name = @categories.find_by(id: params[:category_id])&.name if params[:category_id].present?
   end
 
   def show
@@ -23,39 +18,30 @@ class Dashboard::CoursesController < Dashboard::DashboardController
     @chapters = @course.chapters
     @lessons = Lesson.where(chapter_id: @chapters.pluck(:id))
     @videos = Video.includes(:upload).where(lesson_id: @lessons.pluck(:id))
-    @enrollments = @course.enrollments
-
-    statistics = @course_service.calculate_course_statistics(@course, @videos, @enrollments)
-    @total_duration = statistics[:total_duration]
-
+    @total_duration = @course_service.calculate_course_statistics(@videos)
     @related_courses = @course_service.get_related_courses(@course)
   end
 
   def new
     @course = Course.new
-    @categories = Category.all
   end
 
   def create
-    @course = current_user.courses.build(course_params)
+    @course = current_user.courses.build(permit_params)
 
     if @course.save
       redirect_to dashboard_course_path(@course), notice: 'Course was successfully created.'
     else
-      @categories = Category.all
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-    @categories = Category.all
-  end
+  def edit; end
 
   def update
-    if @course.update(course_params)
+    if @course.update(permit_params)
       redirect_to dashboard_course_path(@course), notice: 'Course was successfully updated.'
     else
-      @categories = Category.all
       render :edit, status: :unprocessable_entity
     end
   end
@@ -71,14 +57,15 @@ class Dashboard::CoursesController < Dashboard::DashboardController
     @course = Course.find(params[:id])
   end
 
+  def set_categories
+    @categories = Category.all
+  end
+
   def initialize_course_service
     @course_service = Dashboard::CourseService.new(current_user)
   end
 
-  def course_params
-    params.require(:course).permit(
-      :title, :description, :price, :thumbnail_path, :language,
-      :status, category_ids: []
-    )
+  def permit_params
+    params.require(:course).permit(:title, :description, :price, :thumbnail_path, :language, :status, category_ids: [])
   end
 end
